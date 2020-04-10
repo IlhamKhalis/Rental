bda5186c172003d452c6f0dacdf57af167437efb
IlhamKhalis <ilham_khalis_27rpl@student.smktelkom-mlg.sch.id>
2020-04-10 18:18:38 +0700

    2

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::post('register', 'PetugasController@register');
+Route::post('login', 'PetugasController@login');
+Route::get('/', function(){
+    return Auth::user()->level;
+})->middleware('jwt.verify');
+
+Route::get('user', 'PetugasController@getAuthenticatedUser')->middleware('jwt.verify');
